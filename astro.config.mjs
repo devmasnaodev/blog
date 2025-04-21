021f8eb48dddd69fe1a2440cc3dc32303f6df75c
Rodrigo Gomes <rodrigo.dev.ux@outlook.com>
2025-04-20 23:12:38 -0300

    feat(config): adiciona alias @ para facilitar importações de componentes
    Configura o Vite para resolver @ como caminho absoluto para ./src.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,17 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
+import path from 'path';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://blog.devmasnaodev.com/",
   integrations: [mdx(), sitemap(), react()],
+  vite: {
+    resolve: {
+      alias: {
+        '@': path.resolve('./src'),
+      },
+    },
+  },
 });
